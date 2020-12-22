2a49fc4fa5c717366854b387b18fbf7247c51262
Лазарев Владислав <v.lazarev@machineheads.ru>
2020-12-22 17:56:39 +0700

    сделал общий элемент для popup

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,48 +3,17 @@ import close from '../images/close.svg';
 function PopUpWithForm(props) {
 
   return(
-    <div className={`popup popup_type_${props.name}`}>
+    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_is-opened' : ''}`}>
         <div className="popup__content">
           <img
             src={close}
             alt=""
             className="popup__close"
-            onClick={closeAddPlaceClick}
+            onClick={props.onClose}
           />
           <h3 className="popup__title">{props.title}</h3>
           <form className="popup__form" name={props.name}noValidate>
             {props.children}
-            {/* <div className="input-container">
-              <input
-                id="name"
-                type="text"
-                name="name"
-                className="popup__input popup__input_type_name"
-                placeholder="Название"
-                required
-                minLength="2"
-                maxLength="30"
-              />
-              <span id="error-name" className="error-message"></span>
-            </div>
-            <div className="input-container">
-              <input
-                id="url"
-                type="url"
-                name="link"
-                className="popup__input popup__input_type_link-url"
-                placeholder="Ссылка на картинку"
-                required
-              />
-              <span id="error-url" className="error-message"></span>
-            </div>
-            <button
-              type
-              className="button popup__button popup__button_type_new"
-              disabled
-            >
-              +
-            </button> */}
           </form>
         </div>
       </div>
